0517d392818b69953ac814d7ea8146578eca223e
kjeok00 <kjeok00@gmail.com>
2024-06-10 15:03:57 +0900

    success connectiong supabase

--- src/controller/BackApp.js
+++ src/controller/BackApp.js
@@ -3,20 +3,13 @@ import { supabase } from './supabaseClient';
 
 function BackApp() {
   const [menuData, setMenuData] = useState([]);
-
+  const fetchMenuData = async () => {
+    const data = await supabase
+      .from('MenuTable')
+      .select('*');
+    setMenuData(data.data);
+  };
   useEffect(() => {
-    const fetchMenuData = async () => {
-      const { data, error } = await supabase
-        .from('MenuTable')
-        .select('*');
-      if (error) {
-        console.log('Error fetching data: ', error);
-      } else {
-        console.log('Fetched data: ', data);
-        setMenuData(data);
-      }
-    };
-    
     fetchMenuData();
   }, []);
   
@@ -24,7 +17,7 @@ function BackApp() {
     <div className="BackApp">
       <h1>Menu Table</h1>
       <ul>
-        {menuData.map(item => (
+        {menuData?.map(item => (
           <li key={item.id}>
             {item.menu} - {item.category} - {item.price}
             <img src={item.image_url} alt={item.menu} width={100} />
