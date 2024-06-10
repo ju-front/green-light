e9bff994ac86d1778cc77c9fbb8b78ca4665b24b
kjeok00 <kjeok00@gmail.com>
2024-06-10 13:09:24 +0900

    [#17] update table.csv

--- src/controller/supabaseClient.js
+++ src/controller/supabaseClient.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 // const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 // const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
-const supabaseUrl = 'https://zgbikvyincwjemxsgohl.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnYmlrdnlpbmN3amVteHNnb2hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4MTY3MTgsImV4cCI6MjAzMzM5MjcxOH0.iw-w9fq5_ryhvOVSqZyb9v5B1NL28j2ub_NWAKwYksA'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
