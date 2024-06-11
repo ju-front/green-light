0f31d83c0bcbe379792c27bb4dff8fa5ea627bb3
kjack00 <129487583+kjeok00@users.noreply.github.com>
2024-06-12 00:04:08 +0900

    [#28] Reset MarkSoldOut

--- src/controller/BackApp.js
+++ src/controller/BackApp.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CreateReceiptTest from './api/CreateReceiptTest';
 import SignInTest from './api/SignInTest';
+// import MarkSoldOutTest from './api/MarkSoldOutTest';
 
 function App() {
   return (
     <div className="BackApp">
       <CreateReceiptTest />
       <SignInTest />
+      {/* <MarkSoldOutTest /> */}
     </div>
   );
 }
