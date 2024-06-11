549bca8c8b385f176e24bb97ea94e1981faa2f46
kjack00 <129487583+kjeok00@users.noreply.github.com>
2024-06-11 23:30:45 +0900

    [#30] CreateRecipt api 구현

---
+++ src/controller/api/CreateReceiptForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import CreateReceipt from './CreateReceipt';
+
+function CreateReceiptForm() {
+  const handleOrder = async () => {
+    const order = {
+      items: [
+        { name: "focaccia", quantity: 2 },
+        { name: "gelato", quantity: 1 },
+        { name: "panna_cotta", quantity: 3 },
+        { name: "bruschetta", quantity: 3 }
+
+      ],
+      total: 26000
+    };
+
+    const response = await CreateReceipt(order);
+    if (response.error) {
+      console.error('Error:', response.error);
+    } else {
+      console.log('Order successfully created:', response.data);
+    }
+  };
+
+  return (
+    <div className="CreateReceiptForm">
+      <h1>Order System</h1>
+      <button onClick={handleOrder}>Place Order</button>
+    </div>
+  );
+}
+
+export default CreateReceiptForm;
