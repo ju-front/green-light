b2b8c4df1854132a0df9301e0ea28f72785a9852
21divcert <zsx2000@naver.com>
2024-06-11 21:45:06 +0900

    공용 컴포넌트 Custom/ CustomButtons, CustomTextField 개발 #20

--- src/component/SuccessPageComponent/ActionButtons.jsx
+++ src/component/SuccessPageComponent/ActionButtons.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ActionButtons.css";
+import CustomButton from "../CustomButtons";
 import { useNavigate } from "react-router-dom";
 
 const ActionButtons = () => {
@@ -8,20 +9,26 @@ const ActionButtons = () => {
     navigate("/");
   };
   const ActionButtonsClickJoin = () => {
-    //StatusDiplay에서 입력한 이름을 받아와서 검증하는 로직이 필요
+    //StatusDisplay에서 입력한 이름을 받아와서 검증하는 로직이 필요
     //검증이 완료되면 아래 코드 실행
     //회원가입이 완료되었다는 메시지를 띄우고 이름 다시 입력 후
     //화살표 버튼을 누르면 PointStatus로 넘어가는 로직이 필요
   };
 
   return (
-    <div>
-      <button className='cancel-button' onClick={ActionButtonsClickCancel}>
-        건너뛰기
-      </button>
-      <button className='confirm-button' onClick={ActionButtonsClickJoin}>
-        회원가입
-      </button>
+    <div className='action-buttons'>
+      <CustomButton
+        text='취소'
+        size='large'
+        color='red'
+        onClick={ActionButtonsClickCancel}
+      />
+      <CustomButton
+        text='회원가입'
+        size='large'
+        color='green'
+        onClick={ActionButtonsClickJoin}
+      />
     </div>
   );
 };
