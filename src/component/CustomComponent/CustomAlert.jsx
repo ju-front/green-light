00163d0373b831ebe141d0d85a8e2e0ee8b9b4bd
21divcert <Hiruca3220@gmail.com>
2024-06-27 20:55:42 +0900

    #20 CustomAlert 컴포넌트 폰트컬러 파람 속성 추가

--- src/component/CustomComponent/CustomAlert.jsx
+++ src/component/CustomComponent/CustomAlert.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./CustomAlert.css";
 
-const CustomAlert = ({ message, duration }) => {
+const CustomAlert = ({ message, duration, fontColor }) => {
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
@@ -15,12 +15,23 @@ const CustomAlert = ({ message, duration }) => {
 
   if (!visible) return null;
 
-  return <div className='custom-alert'>{message}</div>;
+  return (
+    <div className='custom-alert' style={{ color: fontColor }}>
+      {message}
+    </div>
+  );
 };
 
 CustomAlert.propTypes = {
   message: PropTypes.string.isRequired,
   duration: PropTypes.number.isRequired,
+  fontColor: PropTypes.string.isRequired,
+};
+
+CustomAlert.defaultProps = {
+  message: "Default message",
+  duration: 2000,
+  fontColor: "white",
 };
 
 export default CustomAlert;
