e8d95b036d0f9d305b75081e15310b140ef88e1e
ambition-kwon <prince5390@gmail.com>
2024-06-27 21:58:03 +0900

    [modify] solved collision

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -5,10 +5,20 @@ const DataContext = createContext(undefined);
 export const DataProvider = ({ children }) => {
   const [receiptID, setReceiptID] = useState();
   const [username, setUsername] = useState("khw");
+  const [allergyData, setAllergyData] = useState({
+    "gluten": false,
+    "dairy": false,
+    "egg": false,
+    "shellfish": false,
+    "nut": false,
+    "soy": false,
+    "fish": false,
+    "celery": false,
+    "mustard": false
+  });
 
-  // 2. 위에서 추가한 변수들을 value={{ 여기 }}에 삽입합니다.(끝)
   return (
-    <DataContext.Provider value={{ username, setUsername, receiptID, setReceiptID }}>
+    <DataContext.Provider value={{ username, setUsername, allergyData, setAllergyData, receiptID, setReceiptID }}>
       {children}
     </DataContext.Provider>
   );
