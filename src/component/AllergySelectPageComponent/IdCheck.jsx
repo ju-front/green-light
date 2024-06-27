54b3d78044d77114d8b60fb27982f56b6c388917
Green-JEONG <th2gr22n@gmail.com>
2024-06-27 23:18:45 +0900

    AllergySelectPage SignIn 메소드 연결 #64

--- src/component/AllergySelectPageComponent/IdCheck.jsx
+++ src/component/AllergySelectPageComponent/IdCheck.jsx
@@ -2,18 +2,32 @@ import React, { useState } from 'react';
 import './IdCheck.css';
 import SignInComponent from './SignInComponent';
 import CustomAlert from '../CustomComponent/CustomAlert';
+import SignIn from '../../controller/api/auth/SignIn'; // Adjust the import path as necessary
+import { useGlobalData } from '../../context/DataContext'; // Adjust the import path as necessary
 
 const IdCheck = () => {
   const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const { setAllergyData } = useGlobalData();
 
-  const handleLoginClick = () => {
-    setShowAlert(true);
+  const handleLoginClick = (userID) => {
+    SignIn(userID).then((response) => {
+      if (response.error) {
+        console.error(response.error);
+        setAlertMessage('아이디가 없습니다.');
+        setShowAlert(true);
+        return;
+      }
+      setAllergyData(response.user);
+      setAlertMessage('로그인 되었습니다.');
+      setShowAlert(true);
+    });
   };
 
   return (
     <div className="idCheckComponent">
       <SignInComponent onLoginClick={handleLoginClick} />
-      {showAlert && <CustomAlert message="로그인 되었습니다." duration={3000} />}
+      {showAlert && <CustomAlert message={alertMessage} duration={3000} />}
     </div>
   );
 };
--- src/component/AllergySelectPageComponent/SignInComponent.jsx
+++ src/component/AllergySelectPageComponent/SignInComponent.jsx
@@ -5,6 +5,10 @@ import CustomTextField from '../CustomComponent/CustomTextField';
 const SignInComponent = ({ onLoginClick }) => {
   const [inputValue, setInputValue] = useState('');
 
+  const handleLogin = () => {
+    onLoginClick(inputValue);
+  };
+
   return (
     <div className="signInComponent">
       <div className="signInBox">
@@ -17,7 +21,7 @@ const SignInComponent = ({ onLoginClick }) => {
           fontSize="15px"
           placeholder="아이디를 입력해주세요."
         />
-        <button className="loginButton" onClick={onLoginClick}>로그인</button>
+        <button className="loginButton" onClick={handleLogin}>로그인</button>
       </div>
     </div>
   );
