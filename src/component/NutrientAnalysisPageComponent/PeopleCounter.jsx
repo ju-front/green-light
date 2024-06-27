cbccc690c63c97b7c77c59259a85c07c3f7b857e
Green-JEONG <th2gr22n@gmail.com>
2024-06-28 00:09:24 +0900

    NutrientAnalysisPage 영양성분 명 수에 따라 계산 #68

--- src/component/NutrientAnalysisPageComponent/PeopleCounter.jsx
+++ src/component/NutrientAnalysisPageComponent/PeopleCounter.jsx
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import './PeopleCounter.css';
 
-const PeopleCounter = () => {
-  const [count, setCount] = useState(1);
-
+const PeopleCounter = ({setPeopleCount, peopleCount}) => {
   const handleMinus = () => {
-    setCount(prevCount => Math.max(prevCount - 1, 1));
+    setPeopleCount(prevCount => Math.max(prevCount - 1, 1));
   };
 
   const handlePlus = () => {
-    setCount(prevCount => prevCount + 1);
+    setPeopleCount(prevCount => prevCount + 1);
   };
 
   return (
     <div className="people-count">몇명이서 드시나요?
       <span className="people-count-controls">
         <button className="minus-button" onClick={handleMinus}>-</button>
-        <span className="people-count-text">{count}명</span>
+        <span className="people-count-text">{peopleCount}명</span>
         <button className="plus-button" onClick={handlePlus}>+</button>
       </span>
     </div>
