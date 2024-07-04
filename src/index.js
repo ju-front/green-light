d15a9d45aee49771f937520471c46cac6e3aab86
ambition-kwon <prince5390@gmail.com>
2024-07-04 16:21:27 +0900

    [add] github pages route basename

--- src/index.js
+++ src/index.js
@@ -42,15 +42,16 @@ const router = createBrowserRouter([
     path: "/*",
     element: <PublicComponentPage />,
   },
-]);
+],
+  {
+    basename: '/green-light',
+  });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <DataProvider>
       <RouterProvider router={router} />
     </DataProvider>
-  </React.StrictMode>
 );
 
 reportWebVitals();
